fa7a6eb26946ec5a4a4b3ea93258446fe0ccd549
Maria Polyakova <maria.polyakova@snowflake.com>
2019-01-11 17:09:24 -0800

    Changed table name on azure login stats

--- src/sar-azure-login/writeBack.py
+++ src/sar-azure-login/writeBack.py
@@ -10,12 +10,12 @@ reader = csv.reader(sys.stdin)
 columns = ["%s STRING" %col if len(col)>2 else "ID_COL STRING" for col in  next(reader)]
 
 values_list = ['(' + ','.join(["'%s'" % x for x in data]) + ')' for data in reader]
-query= "insert into SNOWALERT.DATA.PROCESS_BASELINE VALUES %s" %', '.join(values_list)
+query= "insert into SNOWALERT.DATA.AZURE_LOGIN_STATS VALUES %s" %', '.join(values_list)
 print(query)
-create_query = "CREATE OR REPLACE TABLE SNOWALERT.DATA.PROCESS_BASELINE (%s)" %','.join(columns )
+create_query = "CREATE OR REPLACE TABLE SNOWALERT.DATA.AZURE_LOGIN_STATS (%s)" %','.join(columns )
 conn = db.connect()
 conn.cursor().execute("USE ROLE APP_SNOWALERT;")
-conn.cursor().execute("DROP TABLE IF EXISTS SNOWALERT.DATA.PROCESS_BASELINE")
+conn.cursor().execute("DROP TABLE IF EXISTS SNOWALERT.DATA.AZURE_LOGIN_STATS")
 conn.cursor().execute(create_query)
 conn.cursor().execute(query)
 conn.cursor().close()
